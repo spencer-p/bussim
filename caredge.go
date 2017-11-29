cf438149c2f6a9311b1ef3255737a059d3324922
Spencer Peterson <spencerjpeterson@gmail.com>
2017-11-28 20:05:30 -0800

    bug fixes for car time

--- caredge.go
+++ caredge.go
@@ -5,8 +5,13 @@ import (
 	"math"
 )
 
-// Car length in miles (15ft/1mi)
-const CARLENGTH = 15 / 5280
+const (
+	// Car length in miles (15ft/1mi)
+	CARLENGTH = 15.0 / 5280.0
+
+	// Capacity k value - calculated from 300ft stop at 60mph
+	KCAPACITY = 3
+)
 
 type CarEdge struct {
 	ToIntersection, FromIntersection string
@@ -36,29 +41,29 @@ func (e *CarEdge) Weight(ta traffic.Agent) float64 {
 
 func (e *CarEdge) Time() int {
 	// Road capacity percentage is inversely related to the speed limit.
-	// 20 mph - half the road can be filled. 40 mph - 25% can be filled. etc
-	roadCapacityPerc := 10 / e.SpeedLimit
+	roadCapacityPerc := KCAPACITY / e.SpeedLimit
 
 	// Actual capactiy of car real estate over road real estate
 	actualCapacityPerc := (CARLENGTH * e.CarCount) / (e.Distance * e.LaneCount)
 
+	// Max capacity will be the optimal road capacity, doubled
+	maxCapacityPerc := 2 * roadCapacityPerc
+
+	minTime := int(e.Distance / (e.SpeedLimit / 60))
+	maxTime := int(e.Distance / (1.0 / 60.0))
+
 	if actualCapacityPerc <= roadCapacityPerc {
 		// If the cars have not exceeded capacity, travel at speed limit
-		return int(e.Distance / (e.SpeedLimit / 60))
-	} else if actualCapacityPerc > 1 {
+		return minTime
+	} else if actualCapacityPerc > maxCapacityPerc {
 		// If they have exceeded 100% road space:
 		// Road is at more than capacity! Speed limit is 1
-		return int(e.Distance)
+		return maxTime
 	} else {
 		// Road is congested. Decrease speed by exceeded capacity.
-
-		// Create custum fn to map speed loss
-		// Width is max road capacity to 100%,
-		// max is real speedlimit (with 1 mph to spare)
-		lostSpeedFn := quadGenerator(1-roadCapacityPerc, e.SpeedLimit-1)
-
-		// The speed is calculated as speed limit - lost speed
-		return int(e.Distance / ((e.SpeedLimit - lostSpeedFn(actualCapacityPerc-roadCapacityPerc)) / 60))
+		// Create quadratic fn to map extra capacity to extra time
+		speedFn := quadGenerator(roadCapacityPerc, float64(maxTime-minTime))
+		return minTime + int(speedFn(actualCapacityPerc-roadCapacityPerc))
 	}
 }
 
--- util.go
+++ util.go
@@ -4,13 +4,17 @@ import "math"
 
 // https://www.desmos.com/calculator/yex502ot5u
 func quadGenerator(width, height float64) func(float64) float64 {
-	mid := (1 / 2) * width
+	mid := 0.5 * width
 	stretch := height / (2 * math.Pow(mid, 2))
 	return func(x float64) float64 {
-		if x >= mid {
+		if x <= 0 {
+			return 0
+		} else if x <= mid {
 			return stretch * math.Pow(x, 2)
-		} else {
+		} else if x < width {
 			return -stretch*math.Pow(x-2*mid, 2) + height
+		} else {
+			return height
 		}
 	}
 }
